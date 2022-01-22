183abed41650d216e69d24770a137574c917205c
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-01-22 14:06:29 +0530

    add start in script

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const  connect  = require("./configs/db");
 const app = require("./index");
-app.listen(3454, async () => {
+const port = process.env.PORT || 3454
+
+app.listen(port, async () => {
     await connect();
     console.log("listning on port 3454");
 });
